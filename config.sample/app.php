2c1eb60c2165f0fca298fa56929f01974749991f
lzs9605 <lzs9605@gmail.com>
2023-07-10 02:16:35 +0800

    update

--- config.sample/app.php
+++ config.sample/app.php
@@ -4,7 +4,7 @@ defined('IN_LitePhp') or exit('Access Denied');
 
 return[
     'ENVIRONMENT'   => 'DEV',  // 'DEV', 'PRO'
-    'APP_DEBUG'     => true,
+    'APP_DEBUG'     => true,  //生产环境：ENVIRONMENT 设为 PRO, APP_DEBUG 设为 false
     'name' => 'LiteApi',
     'authkey' => 'LitePhp_185622a8f4e2c72a9f75f8f5b8099259',
     'rsaKey'        => [
